7f9cafe2a07b08f456d277dabcff52f3555596d7
Tuhin Bepari <digitaldreams40@gmail.com>
2020-07-27 14:43:22 +0600

    Purge CSS is now working

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,26 @@
-let tailwindcss = require('tailwindcss');
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+    // Specify the paths to all of the template files in your project
+    content: [
+        './templates/**/*.html.twig',
+    ],
+
+    // This is the function used to extract class names from your templates
+    defaultExtractor: content => {
+        // Capture as liberally as possible, including things like `h-(screen-1.5)`
+        const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+
+        // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+        const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+
+        return broadMatches.concat(innerMatches)
+    }
+})
 
 module.exports = {
     plugins: [
-        tailwindcss('tailwind.config.js'),
+        require('tailwindcss'),
+        require('autoprefixer'),
+        purgecss,
     ]
 }
