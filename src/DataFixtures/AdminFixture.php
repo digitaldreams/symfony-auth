0f946330e2dfbdd712a0d0a6ce4071fdc2f6993a
Tuhin Bepari <digitaldreams40@gmail.com>
2020-07-28 16:39:24 +0600

    Adding Admin Fixture to create an admin user

---
+++ src/DataFixtures/AdminFixture.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AdminFixture extends Fixture
+{
+    protected $passwordEncoder;
+
+    /**
+     * UserFixture constructor.
+     *
+     * @param \Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface $passwordEncoder
+     */
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    /**
+     * @param \Doctrine\Persistence\ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $admin = new User();
+        $admin->setName('System Admin');
+        $admin->setEmail('admin@admin.com');
+        $admin->setUsername('admin');
+        $admin->setPassword($this->passwordEncoder->encodePassword($admin, 'admin'));
+        $admin->setRoles(['ROLE_ADMIN']);
+        $manager->persist($admin);
+        $manager->flush();
+    }
+}
