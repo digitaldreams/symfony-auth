f4facc8ceb0b0f0eea25f8ae52955f736de00744
Tuhin Bepari <digitaldreams40@gmail.com>
2026-01-09 11:21:46 +0600

    Upgrade to Symfony 7.4

--- src/Security/UserChecker.php
+++ src/Security/UserChecker.php
@@ -5,6 +5,7 @@ namespace App\Security;
 use App\Entity\User;
 use Symfony\Component\Security\Core\Exception\AccountExpiredException;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\User\UserCheckerInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
@@ -21,7 +22,7 @@ class UserChecker implements UserCheckerInterface
         }
     }
 
-    public function checkPostAuth(UserInterface $user): void
+    public function checkPostAuth(UserInterface $user, ?TokenInterface $token = null): void
     {
         //  if (!$user->isActive())) {
         // throw new CustomUserMessageAccountStatusException('Your user account is $status.');
